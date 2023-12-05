707704581fbf81c1df6b97e4ffbc7427541c1f24
gjwilliams716 <gjwilliams716@gmail.com>
2023-12-05 15:41:10 -0600

    Update get-tackles.php

--- get-tackles.php
+++ get-tackles.php
@@ -2,13 +2,13 @@
 
 // Your data retrieval logic here, for example, fetching total tackles data from a database
 // Replace 'your_database_credentials' with your actual database connection details
-$mysqli = new mysqli('');
-$query = "SELECT player_name, total_tackles FROM ou_football_players";
+$mysqli = new mysqli('159.89.47.44', 'admin', 'FinalProject4013', 'willmiso_finalproject');
+$query = "SELECT player, tot FROM oudtbl";
 $result = $mysqli->query($query);
 
 $data = array();
 while ($row = $result->fetch_assoc()) {
-    $data[] = array('name' => $row['player_name'], 'y' => (float)$row['total_tackles']);
+    $data[] = array('name' => $row['name'], 'y' => (float)$row['tot']);
 }
 
 // Convert the data to JSON and send it to the client
