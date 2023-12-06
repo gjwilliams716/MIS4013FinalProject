2217a4f86c840632f98d460db00263ddc74e9fcc
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-12-05 20:42:55 -0600

    Update oudtbl.php

--- oudtbl.php
+++ oudtbl.php
@@ -3,6 +3,8 @@ require_once("util-db.php");
 require_once("model-oudtbl.php");
 
 $pageTitle = "oudtbl";
+include "view-header.php";
 $oudtbl = selectOudtbl();
 include "view-oudtbl.php";
+include "view-footer.php";
 ?>
