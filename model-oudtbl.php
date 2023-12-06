42a25063f80b368ccf27d57c7ca12471b79c8995
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-12-05 21:20:36 -0600

    Update model-oudtbl.php

--- model-oudtbl.php
+++ model-oudtbl.php
@@ -3,7 +3,6 @@ function selectOudtbl() {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT rk, player, g, solo, ast, tot, loss, sk, inte, fr, ff FROM `oudtbl`");
-        $stmt->bind_param("s", $uEmail);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
