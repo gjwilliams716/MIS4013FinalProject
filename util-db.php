1948fdd2ccb8688771cf3a8486e9a2cb44aeff0a
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-12-05 21:19:09 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli('159.89.47.44', 'admin', 'FinalProject4013', 'willmiso_finalproject');
+    $conn = new mysqli('159.89.47.44', 'willmiso_admin', 'FinalProject4013', 'willmiso_finalproject');
     
     // Check connection
     if ($conn->connect_error) {
