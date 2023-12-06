562e1c2b308143b90fb4b8d6076218db85596e95
gjwilliams716 <gjwilliams716@gmail.com>
2023-12-05 23:02:26 -0600

    Update chart.php

--- chart.php
+++ chart.php
@@ -11,11 +11,13 @@ $oudtbl = selectChart();
 $chart2Data = selectChart2(); // Call the function to get data for the second chart
 
 ?>
+<div style="display:flex; flex-direction:column;">
   <h1 style="font-weight: bold; margin-left:10px;">2023 Oklahoma Football Sack Leaders</h1>
 <?php
 include "view-chart.php";
 ?>
   <h1 style="font-weight: bold; margin-left:10px;">2023 Oklahoma Football Fumble Leaders</h1>
+</div>
 <?php
 
 include "view-chart2.php"; // Include the view for the second chart
