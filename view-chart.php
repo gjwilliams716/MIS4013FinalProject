4a01fab55e2bbde33a83a0a05aca6b4ee1bab304
gjwilliams716 <gjwilliams716@gmail.com>
2023-12-05 22:11:57 -0600

    Update view-chart.php

--- view-chart.php
+++ view-chart.php
@@ -1,23 +1,60 @@
-<h1 style="font-weight:bold;">2023 Oklahoma Football Sack Leaders</h1>
-<div class = "table-responsive">
-<table class="table">
-<thead>
-  <tr>
-    <th>Player</th>
-    <th>Sack</th>
-  </tr>
-</thead>
-<tbody>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Football Sack Leaders - Bar Chart</title>
+  <!-- Include Chart.js library -->
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  <style>
+    canvas {
+      display: block;
+      margin: 20px auto;
+    }
+  </style>
+</head>
+<body>
+  <h1 style="font-weight: bold;">2023 Oklahoma Football Sack Leaders</h1>
+  <canvas id="barChart" width="400" height="200"></canvas>
+
   <?php
-while ($oudtbls = $oudtbl->fetch_assoc()) {
+  // Assuming $oudtbl is your result set
+  $labels = [];
+  $data = [];
+
+  while ($oudtbls = $oudtbl->fetch_assoc()) {
+    $labels[] = $oudtbls['player'];
+    $data[] = $oudtbls['sk'];
+  }
   ?>
-  <tr>
-  <td><?php echo $oudtbls['player']; ?></td>
-    <td><?php echo $oudtbls['sk']; ?></td>
-  </tr>
-  <?php
-}
-?>
-</tbody>
-</table>
-</div>
+
+  <script>
+    // Get the PHP data and convert it to JavaScript arrays
+    var labels = <?php echo json_encode($labels); ?>;
+    var data = <?php echo json_encode($data); ?>;
+
+    // Create a bar chart using Chart.js
+    var ctx = document.getElementById('barChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Sack',
+          data: data,
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderColor: 'rgba(75, 192, 192, 1)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  </script>
+</body>
+</html>
