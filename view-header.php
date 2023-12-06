ca19f7fbde975bfc047fc10cc710725ab0415b0b
gjwilliams716 <gjwilliams716@gmail.com>
2023-12-05 22:25:42 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -7,17 +7,20 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
-     <div id="topo" style="height:100px; background-image:URL(https://images.squarespace-cdn.com/content/v1/5b99b818620b8562c2caa34a/1541298742511-AHQQITXDKMJ9ZURXLVCK/Monthly-Website-Header-background.jpg?format=2500w); background-position:center;">
+      <div id="topo" style="height:100px; background-image:URL(https://images.squarespace-cdn.com/content/v1/5b99b818620b8562c2caa34a/1541298742511-AHQQITXDKMJ9ZURXLVCK/Monthly-Website-Header-background.jpg?format=2500w); background-position:center;">
     <div style="font-family:arial; font-size:30px; font-weight:bold; text-align:center; color:white;">MIS 4013 Final Project</div>
 
     <div id="navi" style="display: flex; justify-content: center; align-items:center; margin-bottom:10px;">
     <ul class="nav">
         <li class="nav-item" style="margin-right: 10px;">
-          <button type="button" class="btn btn-secondary btn-sm"><a class="nav-link active" style="color:white; font-size: 75%; font-weight:bold;" href="index.html">Homepage</a></button>
+          <button type="button" class="btn btn-secondary btn-sm"><a class="nav-link active" style="color:white; font-size: 75%; font-weight:bold;" href="index.html">Project Overview</a></button>
+        </li>
+      <li class="nav-item" style="margin-right: 10px;">
+          <button type="button" class="btn btn-secondary btn-sm"><a class="nav-link" style="color:white; font-size: 75%; font-weight:bold;" href="chart.php">Stat Leaders</a></button>
         </li>
         <li class="nav-item">
-          <button type="button" class="btn btn-secondary btn-sm"><a class="nav-link" style="color:white; font-size: 75%; font-weight:bold;" href="oudtbl.php">More Statistics</a></button>
-        </li>
+          <button type="button" class="btn btn-secondary btn-sm"><a class="nav-link" style="color:white; font-size: 75%; font-weight:bold;" href="oudtbl.php">Defensive Dataset</a></button>
+        </li> 
     </ul>
     </div>
    </div>
