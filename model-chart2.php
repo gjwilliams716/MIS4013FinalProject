a27646684222b6a31876baac826cd08d313e3f7e
gjwilliams716 <gjwilliams716@gmail.com>
2023-12-05 22:40:53 -0600

    Update model-chart2.php

--- model-chart2.php
+++ model-chart2.php
@@ -2,7 +2,7 @@
 function selectChart2() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT player, ff FROM `oudtbl` WHERE ff > 1");
+        $stmt = $conn->prepare("SELECT player, ff FROM `oudtbl` WHERE ff > 0");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
