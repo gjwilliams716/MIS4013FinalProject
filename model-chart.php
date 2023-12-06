a5e9ff014e91c49e3fb66a94b465059fb8c323db
gjwilliams716 <gjwilliams716@gmail.com>
2023-12-05 21:39:49 -0600

    Create model-chart.php

---
+++ model-chart.php
@@ -0,0 +1,14 @@
+<?php
+function selectOudtbl() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT player, sk FROM `oudtbl`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
