7b4678f0462d6979e7cf0af88c8dfc9fe92a8311
gjwilliams716 <gjwilliams716@gmail.com>
2023-12-05 22:37:09 -0600

    Update chart.php

--- chart.php
+++ chart.php
@@ -1,12 +1,16 @@
 <?php
 require_once("util-db.php");
 require_once("model-chart.php");
+require_once("model-chart2.php"); // Include the second chart model file
 
 $pageTitle = "oudtbl";
 include "view-header.php";
+
 $oudtbl = selectChart();
+$chart2Data = selectChart2(); // Call the function to get data for the second chart
+
 include "view-chart.php";
-$oudtbl2 = selectChart2();
-include "view-chart2.php";
+include "view-chart2.php"; // Include the view for the second chart
+
 include "view-footer.php";
 ?>
