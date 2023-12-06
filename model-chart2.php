8a0cfee36202fb408e495121189d86a821b370c8
gjwilliams716 <gjwilliams716@gmail.com>
2023-12-05 22:29:04 -0600

    Create model-chart2.php

---
+++ model-chart2.php
@@ -0,0 +1,14 @@
+<?php
+function selectChart2() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT player, ff FROM `oudtbl` WHERE ff > 1");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
