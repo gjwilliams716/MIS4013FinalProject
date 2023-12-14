5cdab318a1c1e7f66730439db0af264d2aaf58fd
gjwilliams716 <gjwilliams716@gmail.com>
2023-12-14 15:48:43 -0600

    Update view-players-form.php

--- view-players-form.php
+++ view-players-form.php
@@ -6,7 +6,6 @@
   </svg>
 </button>
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 <!-- Modal -->
 <div class="modal fade" id="newPlayer" tabindex="-1" role="dialog" aria-labelledby="newPlayerLabel" aria-hidden="true">
