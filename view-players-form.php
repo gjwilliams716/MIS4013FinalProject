4e998f594ed2ea593fd0ae2f076c67f8e7ed0d57
gjwilliams716 <gjwilliams716@gmail.com>
2023-12-14 15:53:00 -0600

    Update view-players-form.php

--- view-players-form.php
+++ view-players-form.php
@@ -1,11 +1,13 @@
+<!-- Add the necessary Bootstrap and jQuery dependencies -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
 <!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#newPlayer">
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill-add" viewBox="0 0 16 16">
-    <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0m-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-    <path d="M2 13c0 1 1 1 1 1h5.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.544-3.393C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4"/>
-  </svg>
+  <!-- SVG Icon Code -->
 </button>
-
 
 <!-- Modal -->
 <div class="modal fade" id="newPlayer" tabindex="-1" role="dialog" aria-labelledby="newPlayerLabel" aria-hidden="true">
@@ -18,19 +20,17 @@
         </button>
       </div>
       <div class="modal-body">
-      
-      <form method="post" action="">
-        <div class="mb-3">
-          <label for="player" class="form-label">Player Name</label>
-          <input type="text" class="form-control" id="player" name="player">
-        </div>
-        <div class="mb-3">
-          <label for="g" class="form-label">Games Played</label>
-          <input type="text" class="form-control" id="player" name="g">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
-        
+        <form method="post" action="">
+          <div class="mb-3">
+            <label for="playerName" class="form-label">Player Name</label>
+            <input type="text" class="form-control" id="playerName" name="player">
+          </div>
+          <div class="mb-3">
+            <label for="gamesPlayed" class="form-label">Games Played</label>
+            <input type="text" class="form-control" id="gamesPlayed" name="g">
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
       </div>
     </div>
   </div>
