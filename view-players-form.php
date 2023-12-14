ce9b37f92d4f693f8538b39d9aec2f11ac36da24
gjwilliams716 <gjwilliams716@gmail.com>
2023-12-14 15:40:49 -0600

    Update view-players-form.php

--- view-players-form.php
+++ view-players-form.php
@@ -17,11 +17,7 @@
         </button>
       </div>
       <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        
       </div>
     </div>
   </div>
