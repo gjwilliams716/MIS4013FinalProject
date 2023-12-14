83322c51e7914ff37428f15f89fcf55e6247287d
gjwilliams716 <gjwilliams716@gmail.com>
2023-12-14 15:50:23 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -6,7 +6,15 @@
     <title><?=$pageTitle?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> <!-- Chart JS -->
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script> <!-- Apex Charts -->
+    <script src="https://cdn.jsdelivr.net/npm/gridjs@1.4.0/dist/gridjs.production.min.js"></script> <!-- Grid JS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/gridjs@1.4.0/dist/theme/mermaid.min.css" />  <!-- Grid JS -->
+    <script src="https://code.highcharts.com/highcharts.js"></script> <!-- High Charts -->
+    <link href="https://unpkg.com/tabulator-tables@5.5.2/dist/css/tabulator.min.css" rel="stylesheet"> <!-- Tabulator -->
+    <script type="text/javascript" src="https://unpkg.com/tabulator-tables@5.5.2/dist/js/tabulator.min.js"></script> <!-- Tabulator -->
   </head>
   <body>
       <div id="topo" style="height:100px; background-image:URL(https://images.squarespace-cdn.com/content/v1/5b99b818620b8562c2caa34a/1541298742511-AHQQITXDKMJ9ZURXLVCK/Monthly-Website-Header-background.jpg?format=2500w); background-position:center;">
