5867cc124da9e4f8a418695f0b71206dcb9c063b
gjwilliams716 <gjwilliams716@gmail.com>
2023-12-14 16:12:48 -0600

    Update oudtbl.php

--- oudtbl.php
+++ oudtbl.php
@@ -4,6 +4,16 @@ require_once("model-oudtbl.php");
 
 $pageTitle = "oudtbl";
 include "view-header.php";
+
+if (isset($_POST['actionType']))
+{
+    switch ($_POST['actionType']) {
+      case "Add";
+      addPlayer($_POST['player'], $_POST['g'], $_POST['solo'], $_POST['ast'], $_POST['tot'], $_POST['loss'], $_POST['sk'], $_POST['inte'], $_POST['fr'], $_POST['ff']);
+    break;
+    }
+}
+
 $oudtbl = selectOudtbl();
 include "view-oudtbl.php";
 include "view-footer.php";
