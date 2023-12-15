c0a3d1e875df488e977e494a50834311050662c9
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-12-14 22:20:32 -0600

    Update oudtbl.php

--- oudtbl.php
+++ oudtbl.php
@@ -8,7 +8,7 @@ include "view-header.php";
 if (isset($_POST['actionType']))
 {
     switch ($_POST['actionType']) {
-      case "Add";
+      case "Add":
       if (addPlayer($_POST['player'], $_POST['g'], $_POST['solo'], $_POST['ast'], $_POST['tot'], $_POST['loss'], $_POST['sk'], $_POST['inte'], $_POST['fr'], $_POST['ff'])){
           echo '<div class="alert alert-success" role="alert">Player added.</div>';
     } else {
