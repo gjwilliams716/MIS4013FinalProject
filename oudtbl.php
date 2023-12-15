8fc9cd026a4183dd2e1a7ec6e8e437357d3c562f
gjwilliams716 <gjwilliams716@gmail.com>
2023-12-14 23:23:32 -0600

    Update oudtbl.php

--- oudtbl.php
+++ oudtbl.php
@@ -16,7 +16,7 @@ if (isset($_POST['actionType'])) {
             break;
         
         case "Edit":
-            if (updatePlayer($_POST['player'], $_POST['g'], $_POST['solo'], $_POST['ast'], $_POST['tot'], $_POST['loss'], $_POST['sk'], $_POST['inte'], $_POST['fr'], $_POST['ff'])) {
+            if (editPlayer($_POST['player'], $_POST['g'], $_POST['solo'], $_POST['ast'], $_POST['tot'], $_POST['loss'], $_POST['sk'], $_POST['inte'], $_POST['fr'], $_POST['ff'])) {
                 echo '<div class="alert alert-success" role="alert">Player edited.</div>';
             } else {
                 echo '<div class="alert alert-danger" role="alert">Error when editing playing.</div>';
