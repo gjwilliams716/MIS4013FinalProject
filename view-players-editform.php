256590c65397bc7051cb50d0c85c13b566750260
Parker-McGowan <142837417+Parker-McGowan@users.noreply.github.com>
2023-12-14 21:53:16 -0600

    Update view-players-editform.php

--- view-players-editform.php
+++ view-players-editform.php
@@ -1,4 +1,4 @@
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editKeeperModal<?php echo $keeper['keeper_id']; ?>">
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editKeeperModal<?php echo $outbl['player']; ?>">
  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
   <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
   <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
@@ -10,7 +10,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="editOutblModalLabel<?php echo $keeper['keeper_id']; ?>">Edit Keeper</h1>
+        <h1 class="modal-title fs-5" id="editOutblModalLabel<?php echo $outbl['player']; ?>">Edit Outbl</h1>
          lModalLabel<?php echo $outbl['player']; ?>">Edit Player</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
@@ -21,15 +21,15 @@
     <input type="text" class="form-control" id="pFirst<?php echo $outbl['player']; ?>" name="pFirst" value="<?php echo $outbl['first_name']; ?>">
   </div>
 <div class="mb-3">
-    <label for="kLast<?php echo $outbl['player']; ?>" class="form-label">player last name</label>
-    <input type="text" class="form-control" id="kLast<?php echo $outbl['player']; ?>" name="pLast" value="<?php echo $keeper['last_name']; ?>">
+    <label for="pLast<?php echo $outbl['player']; ?>" class="form-label">player last name</label>
+    <input type="text" class="form-control" id="pLast<?php echo $outbl['player']; ?>" name="pLast" value="<?php echo $keeper['last_name']; ?>">
   </div>
           <div class="mb-3">
-    <label for="kContact<?php echo $keeper['player']; ?>" class="form-label">Contact</label>
+    <label for="pContact<?php echo $keeper['player']; ?>" class="form-label">Contact</label>
     <input type="text" class="form-control" id="pContact<?php echo $outbl['player']; ?>" name="pContact" value="<?php echo $outbl['contact']; ?>">
   </div>
       
-   <input type="hidden" name="kid" value="<?php echo $outbl['keeper_id']; ?>">
+   <input type="hidden" name="pid" value="<?php echo $outbl['player']; ?>">
    <input type="hidden" name="actionType" value="Edit">      
   <button type="submit" class="btn btn-primary">Save</button>
 </form>
