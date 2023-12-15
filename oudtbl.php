42330b0bddb67e3c5f0708cc9d181875fb25c3a0
gjwilliams716 <gjwilliams716@gmail.com>
2023-12-14 22:38:13 -0600

    Update oudtbl.php

--- oudtbl.php
+++ oudtbl.php
@@ -5,31 +5,32 @@ require_once("model-oudtbl.php");
 $pageTitle = "oudtbl";
 include "view-header.php";
 
-if (isset($_POST['actionType'])){
+if (isset($_POST['actionType'])) {
     switch ($_POST['actionType']) {
-     case "Add":
-      if (addPlayer($_POST['player'], $_POST['g'], $_POST['solo'], $_POST['ast'], $_POST['tot'], $_POST['loss'], $_POST['sk'], $_POST['inte'], $_POST['fr'], $_POST['ff'])){
-          echo '<div class="alert alert-success" role="alert">Player added.</div>';
-    } else {
-      echo '<div class="alert alert-danger" role="alert">Error.</div>';
-      }
-    break;
+        case "Add":
+            if (addPlayer($_POST['player'], $_POST['g'], $_POST['solo'], $_POST['ast'], $_POST['tot'], $_POST['loss'], $_POST['sk'], $_POST['inte'], $_POST['fr'], $_POST['ff'])) {
+                echo '<div class="alert alert-success" role="alert">Player added.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error.</div>';
+            }
+            break;
         
-    case "Edit":
-    if (updatePlayer($_POST['player'], $_POST['g'], $_POST['solo'], $_POST['ast'], $_POST['tot'], $_POST['loss'], $_POST['sk'], $_POST['inte'], $_POST['fr'], $_POST['ff'])) {
-     echo '<div class="alert alert-success" role="alert">Player edited.</div>';
-    } else {
-      echo '<div class="alert alert-danger" role="alert">Error.</div>';
+        case "Edit":
+            if (updatePlayer($_POST['player'], $_POST['g'], $_POST['solo'], $_POST['ast'], $_POST['tot'], $_POST['loss'], $_POST['sk'], $_POST['inte'], $_POST['fr'], $_POST['ff'])) {
+                echo '<div class="alert alert-success" role="alert">Player edited.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error.</div>';
+            }
+            break;
+
+        case "Delete":
+            if (deletePlayer($_POST['player'])) {
+                echo '<div class="alert alert-success" role="alert">Player deleted.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error.</div>';
+            }
+            break;
     }
-  break;
-    case "Delete":
-    if (deletePlayer($_POST['player'], $_POST['g'], $_POST['solo'], $_POST['ast'], $_POST['tot'], $_POST['loss'], $_POST['sk'], $_POST['inte'], $_POST['fr'], $_POST['ff'])) {
-     echo '<div class="alert alert-success" role="alert">Player deleted.</div>';
-    } else {
-      echo '<div class="alert alert-danger" role="alert">Error.</div>';
-    }
-  break;
-  }
 }
 
 $oudtbl = selectOudtbl();
